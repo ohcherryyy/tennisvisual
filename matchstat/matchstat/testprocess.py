a4ae02fee96e7986849a94367e5ae591faa185bc
ohcherryyy <914120660@qq.com>
2020-03-25 17:38:42 +0800

    ausopengraph+timeline2.0

--- matchstat/matchstat/testprocess.py
+++ matchstat/matchstat/testprocess.py
@@ -4,51 +4,51 @@ from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 
 if __name__ == "__main__":
-    # with DB(db='matchstat') as db:
-    #     db.execute("SELECT `winnername`,`losername`,`round`,`id` FROM `matchstat`.`ausopen` WHERE `year`='2020.01.20 - 2020.02.03' ")
-    #     result=db.fetchall()
-    #     sq="INSERT INTO `matchstat`.`2020ausopenlinks`(`source`,`target`,`value`,`id`) VALUES(%s,%s,%s,%s)"
-    #     for x in range(0,len(result)):
-    #         if(result[x][2]=="Final"):
-    #             db.execute(sq,(result[x][1],result[x][0],"决赛",result[x][3]))
-    #         if(result[x][2]=="Semifinals"):
-    #             db.execute(sq,(result[x][1],result[x][0],"半决赛",result[x][3]))
-    #         if(result[x][2]=="Quarterfinals"):
-    #             db.execute(sq,(result[x][1],result[x][0],"四分之一决赛",result[x][3]))
-    #         if(result[x][2]=="Round of 16"):
-    #             db.execute(sq,(result[x][1],result[x][0],"16强",result[x][3]))
-    #         if(result[x][2]=="Round of 32"):
-    #             db.execute(sq,(result[x][1],result[x][0],"32强",result[x][3]))
-    #         if(result[x][2]=="Round of 64"):
-    #             db.execute(sq,(result[x][1],result[x][0],"64强",result[x][3]))
-    #         if(result[x][2]=="Round of 128"):
-    #             db.execute(sq,(result[x][1],result[x][0],"128强",result[x][3]))
     with DB(db='matchstat') as db:
-        db.execute("SELECT `winnername`,`round` FROM `matchstat`.`ausopen` WHERE `year`='2020.01.20 - 2020.02.03' ")
+        db.execute("SELECT `winnername`,`losername`,`round`,`id` FROM `matchstat`.`ausopen` WHERE `year`='2015.01.19 - 2015.02.01' ")
         result=db.fetchall()
-        
-        db.execute("SELECT `losername` FROM `matchstat`.`ausopen` WHERE `round`='Round of 128' and `year`='2020.01.20 - 2020.02.03'")
-        loser=db.fetchall()
-        
-        sq="INSERT IGNORE INTO `matchstat`.`2020ausopen`(`category`,`name`,`symbolSize`,`symbol`,`value`) VALUES(%s,%s,%s,%s,%s)"
+        sq="INSERT INTO `matchstat`.`2015ausopenlinks`(`source`,`target`,`value`,`id`) VALUES(%s,%s,%s,%s)"
         for x in range(0,len(result)):
-            db.execute("SELECT `img` FROM `matchstat`.`imgofsportsman` WHERE `name`=%s",(result[x][0],))
-            img=db.fetchall()
-            if(result[x][1]=="Final"):
-                db.execute(sq,(0,result[x][0],150,"image://http://"+img[0][0],50))
-            if(result[x][1]=="Semifinals"):
-                db.execute(sq,(1,result[x][0],130,"image://http://"+img[0][0],40))
-            if(result[x][1]=="Quarterfinals"):
-                db.execute(sq,(2,result[x][0],110,"image://http://"+img[0][0],30))
-            if(result[x][1]=="Round of 16"):
-                db.execute(sq,(3,result[x][0],90,"image://http://"+img[0][0],25))
-            if(result[x][1]=="Round of 32"):
-                db.execute(sq,(4,result[x][0],70,"image://http://"+img[0][0],20))
-            if(result[x][1]=="Round of 64"):
-                db.execute(sq,(5,result[x][0],50,"image://http://"+img[0][0],15))
-            if(result[x][1]=="Round of 128"):
-                db.execute(sq,(6,result[x][0],30,"image://http://"+img[0][0],10))
-        for y in range(0,len(loser)):
-            db.execute("SELECT `img` FROM `matchstat`.`imgofsportsman` WHERE `name`=%s",(loser[y][0],))
-            img=db.fetchall()
-            db.execute(sq,(7,loser[y][0],10,"image://http://"+img[0][0],5))    
+            if(result[x][2]=="Finals"):
+                db.execute(sq,(result[x][1],result[x][0],"决赛",result[x][3]))
+            if(result[x][2]=="Semi-Finals"):
+                db.execute(sq,(result[x][1],result[x][0],"半决赛",result[x][3]))
+            if(result[x][2]=="Quarter-Finals"):
+                db.execute(sq,(result[x][1],result[x][0],"四分之一决赛",result[x][3]))
+            if(result[x][2]=="Round of 16"):
+                db.execute(sq,(result[x][1],result[x][0],"16强",result[x][3]))
+            if(result[x][2]=="Round of 32"):
+                db.execute(sq,(result[x][1],result[x][0],"32强",result[x][3]))
+            if(result[x][2]=="Round of 64"):
+                db.execute(sq,(result[x][1],result[x][0],"64强",result[x][3]))
+            if(result[x][2]=="Round of 128"):
+                db.execute(sq,(result[x][1],result[x][0],"128强",result[x][3]))
+    # with DB(db='matchstat') as db:
+    #     db.execute("SELECT `winnername`,`round` FROM `matchstat`.`ausopen` WHERE `year`='2015.01.19 - 2015.02.01' ")
+    #     result=db.fetchall()
+        
+    #     db.execute("SELECT `losername` FROM `matchstat`.`ausopen` WHERE `round`='Round of 128' and `year`='2015.01.19 - 2015.02.01'")
+    #     loser=db.fetchall()
+        
+    #     sq="INSERT IGNORE INTO `matchstat`.`2015ausopen`(`category`,`name`,`symbolSize`,`symbol`,`value`) VALUES(%s,%s,%s,%s,%s)"
+    #     for x in range(0,len(result)):
+    #         db.execute("SELECT `img` FROM `matchstat`.`imgofsportsman` WHERE `name`=%s",(result[x][0],))
+    #         img=db.fetchall()
+    #         if(result[x][1]=="Finals"):
+    #             db.execute(sq,(0,result[x][0],180,"image://http://"+img[0][0],50))
+    #         if(result[x][1]=="Semi-Finals"):
+    #             db.execute(sq,(1,result[x][0],150,"image://http://"+img[0][0],40))
+    #         if(result[x][1]=="Quarter-Finals"):
+    #             db.execute(sq,(2,result[x][0],130,"image://http://"+img[0][0],30))
+    #         if(result[x][1]=="Round of 16"):
+    #             db.execute(sq,(3,result[x][0],110,"image://http://"+img[0][0],25))
+    #         if(result[x][1]=="Round of 32"):
+    #             db.execute(sq,(4,result[x][0],90,"image://http://"+img[0][0],20))
+    #         if(result[x][1]=="Round of 64"):
+    #             db.execute(sq,(5,result[x][0],70,"image://http://"+img[0][0],15))
+    #         if(result[x][1]=="Round of 128"):
+    #             db.execute(sq,(6,result[x][0],50,"image://http://"+img[0][0],10))
+    #     for y in range(0,len(loser)):
+    #         db.execute("SELECT `img` FROM `matchstat`.`imgofsportsman` WHERE `name`=%s",(loser[y][0],))
+    #         img=db.fetchall()
+    #         db.execute(sq,(7,loser[y][0],30,"image://http://"+img[0][0],5))    
---
+++ visualization/creategraph.js
@@ -0,0 +1,292 @@
+function getOption(nodes,links,categoriesname){
+
+    var nodee=[];
+    var linkk=[];
+
+    for(var i=0;i<nodes.length;i++){
+        var nod=[];
+        for(var j=0;j<nodes[i].length;j++){
+        var node={};
+        node['name']=nodes[i][j].name;
+        node['category']=Number(nodes[i][j].category);
+        node['symbolSize']=nodes[i][j].symbolSize;
+        node['symbol']=nodes[i][j].symbol;
+        node['draggable']=true;
+        node['value']=Number(nodes[i][j].value);
+        if(nodes[i][j].category==0)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[0].name+':{b}';
+        }
+        if(nodes[i][j].category==1)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[1].name+':{b}';
+        }
+        if(nodes[i][j].category==2)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[2].name+':{b}';
+        }
+        if(nodes[i][j].category==3)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[3].name+':{b}';
+        }
+        if(nodes[i][j].category==4)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[4].name+':{b}';
+        }
+        if(nodes[i][j].category==5)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[5].name+':{b}';
+        }
+        if(nodes[i][j].category==6)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[6].name+':{b}';
+        }
+        if(nodes[i][j].category==7)
+        {
+            node['emphasis']={};
+            node['emphasis']['label']={};
+            node['emphasis']['label']['show']=true;
+            node['emphasis']['label']['formatter']=categoriesname[7].name+':{b}';
+        }
+        nod.push(node);
+    }
+    nodee.push(nod);
+    };
+
+
+   
+    for(var i=0;i<links.length;i++){
+        var lin=[];
+        for(var j=0;j<links[i].length;j++){
+        var link={};
+        link['source']=links[i][j].source;
+        link['target']=links[i][j].target;
+        link['value']=links[i][j].value;
+        link['id']=links[i][j].id;
+        link['emphasis']={}
+        link['emphasis']['label']={};
+        link['emphasis']['label']['show']=true;
+        link['emphasis']['label']['formatter']='{@value}';
+        lin.push(link);
+        }
+        linkk.push(lin);
+    }
+
+
+    option = {
+        baseOption:{
+            timeline:{
+                axisType:'category',
+                autoPlay:false,
+                playInterval:2000,
+                data:['2020','2019','2018','2017','2016','2015','2014','2013','2012','2011'],
+            },
+            title:{
+                top:'5px'
+            },
+        legend:{
+            show:true,
+            left:'5px',
+            top:'50px',
+            data:categoriesname.map(function (b){
+                return b.name;
+            }),
+            selected:{
+                '64强':false,
+                '128强':false
+            },
+            orient:'vertical',
+            borderColor:'#ccc',
+            borderWidth:1
+            
+        },
+        series : [
+            {
+                type:'graph',
+                layout:"force",
+                force:{
+                    repulsion:500,
+                    gravity:0.07,
+                    edgeLength:[20,400],
+                    layoutAnimation:true
+                },
+                lineStyle: {
+                    width:1,
+                    curveness:0.2,
+                    color:'source'
+                        },
+                animationDurationUpdate: 1500,
+                animationEasingUpdate: 'quinticInOut',
+                itemStyle: { 
+                    emphasis: {
+                        label: {
+                            show: true,
+                            position:'bottom',
+                            formatter:'{b}',
+                        },
+                        lineStyle : {
+                            width:5,
+                            
+                        }
+                    }
+                },
+                focusNodeAdjacency: true,
+                roam: true,
+                categories:categoriesname,
+                nodes: nodee,
+                links : linkk,                
+            }
+        ],
+        },
+            options:[
+                {//2020
+                    title:{
+                        text:'2020澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[0],
+                            links:linkk[0]
+                        }
+                    ]
+                },
+                {//2019
+                    title:{
+                        text:'2019澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[1],
+                            links:linkk[1]
+                        }
+                    ]
+                },
+                {//2018
+                    title:{
+                        text:'2018澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[2],
+                            links:linkk[2]
+                        }
+                    ]
+                },
+                {//2017
+                    title:{
+                        text:'2017澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[3],
+                            links:linkk[3]
+                        }
+                    ]
+                },
+                {//2016
+                    title:{
+                        text:'2016澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[4],
+                            links:linkk[4]
+                        }
+                    ]
+                },
+                {//2015
+                    title:{
+                        text:'2015澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[5],
+                            links:linkk[5]
+                        }
+                    ]
+                },
+                {//2014
+                    title:{
+                        text:'2014澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[6],
+                            links:linkk[6]
+                        }
+                    ]
+                },
+                {//2013
+                    title:{
+                        text:'2013澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[7],
+                            links:linkk[7]
+                        }
+                    ]
+                },
+                {//2012
+                    title:{
+                        text:'2012澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[8],
+                            links:linkk[8]
+                        }
+                    ]
+                },
+                {//2011
+                    title:{
+                        text:'2011澳大利亚网球公开赛（男单）',
+                        fontSize:20
+                    },
+                    series:[
+                        {
+                            nodes:nodee[9],
+                            links:linkk[9]
+                        }
+                    ]
+                },
+            ]
+        
+    };
+    return option;    
+}
+
+function drawGraph(mychart,jsondata){
+    option=getOption(jsondata);
+    mychart.setOption(option);
+}
