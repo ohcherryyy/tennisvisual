5b6d1cb438b61761a05a1bb7cbe92769e8e15003
ohcherryyy <914120660@qq.com>
2020-04-04 20:24:44 +0800

    radar+charts2.0

--- visualization/statcreate.js
+++ visualization/statcreate.js
@@ -92,7 +92,7 @@ function creategraph(statdata,id,winnername,losername){
                 {text:'破发成功率(%)',max:100},
             ],
             center:['25%','50%'],
-            radius:100
+            radius:90
         },
         {
             indicator:[
@@ -102,7 +102,7 @@ function creategraph(statdata,id,winnername,losername){
                 {text:'被破发率(%)',max:100},
             ],
             center:['75%','50%'],
-            radius:100
+            radius:80
         }
 
     ],
@@ -127,4 +127,320 @@ function creategraph(statdata,id,winnername,losername){
         ]
     }
     return option
+}
+
+function servicecreate(statdata,id,winnername,losername){
+    var ace=[]
+    var df=[]
+    var firstsuccess=[]
+    var firstpoints=[]
+    var secondpoints=[]
+    for (var i=0;i<statdata.length;i++){
+        if(statdata[i].id==id){            
+            switch(statdata[i].statslabel){
+                case('Aces'):
+                    ace.push(Number(statdata[i].winner))
+                    ace.push(Number(statdata[i].loser))
+                    break;
+                case('1st Serve'):
+                    firstsuccess.push(Number(statdata[i].wactual))
+                    firstsuccess.push(Number(statdata[i].lactual))
+                    break;
+                case('1st Serve Points Won'):
+                    firstpoints.push(Number(statdata[i].wactual))
+                    firstpoints.push(Number(statdata[i].lactual))
+                    break;
+                case('2nd Serve Points Won'):
+                    secondpoints.push(Number(statdata[i].wactual))
+                    secondpoints.push(Number(statdata[i].lactual))
+                    break;
+                case('Double Faults'):
+                    df.push(Number(statdata[i].winner))
+                    df.push(Number(statdata[i].loser))
+                    break;
+            }
+        }
+    }
+    
+    option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            
+                type: 'shadow' 
+            }
+        },
+        legend: {
+            data: ['Ace球', '双发失误次数', '一发成功次数', '一发得分次数', '二发得分次数']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            show:false
+        },
+        yAxis: {
+            type: 'category',
+            data: [winnername,losername]
+        },
+        series: [
+            {
+                name: 'Ace球',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight',
+
+                },
+                data: ace
+            },
+            {
+                name: '双发失误次数',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight'
+                },
+                data: df
+            },
+            {
+                name: '一发成功次数',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight'
+                },
+                data: firstsuccess
+            },
+            {
+                name: '一发得分次数',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight',
+                    
+                },
+                data: firstpoints
+            },
+            {
+                name: '二发得分次数',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight'
+                },
+                data: secondpoints
+            }
+        ]
+    }
+    return option;
+}
+
+function returncreate(statdata,id,winnername,losername){ 
+    var firstpoints=[]
+    var secondpoints=[]
+    for (var i=0;i<statdata.length;i++){
+        if(statdata[i].id==id){            
+            switch(statdata[i].statslabel){
+                case('1st Serve Return Points Won'):
+                    firstpoints.push(Number(statdata[i].wactual))
+                    firstpoints.push(Number(statdata[i].lactual))
+                    break;
+                case('2nd Serve Return Points Won'):
+                    secondpoints.push(Number(statdata[i].wactual))
+                    secondpoints.push(Number(statdata[i].lactual))
+                    break;
+            }
+        }
+    }
+    
+    option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            
+                type: 'shadow' 
+            }
+        },
+        legend: {
+            data: ['一发接发球得分次数', '二发接发球得分次数']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            show:false
+        },
+        yAxis: {
+            type: 'category',
+            data: [winnername,losername]
+        },
+        series: [
+            {
+                name: '一发接发球得分次数',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight',
+
+                },
+                data: firstpoints
+            },
+            {
+                name: '二发接发球得分次数',
+                type: 'bar',
+                stack: '发球数',
+                label: {
+                    show: true,
+                    position: 'insideRight'
+                },
+                data: secondpoints
+            },
+        ]
+    }
+    return option;
+}
+
+function breakpointscreate(statdata,id,winnername,losername){
+    for (var i=0;i<statdata.length;i++){
+        if(statdata[i].id==id){    
+            if(statdata[i].statslabel=='Break Points Saved')
+            {
+                winner=Number(statdata[i].wstat)
+                wlabel=statdata[i].winner
+                loser=Number(statdata[i].lstat)
+                llabel=statdata[i].loser
+            }
+            }
+        }
+    
+    option = {
+        title:{
+            text:'成功挽救破发点',
+            left:'auto'
+        },
+        series: [
+            {
+                name: 'winner',
+                type: 'pie',
+                radius: ['60%','80%'],
+                center: ['40%', '50%'],
+                data: [
+                    {value:winner},
+                    {value:100-winner}
+                ],
+                labelLine:{
+                    show:false
+                },
+                label:{
+                    show:true,
+                    position:'center',
+                    formatter:winnername+':\n'+wlabel,
+                    fontSize:15,
+                    backgroundColor:'rgba(255,255,255,0.5)'
+                },
+            },
+            {
+                name: 'loser',
+                type: 'pie',
+                radius: ['60%','80%'],
+                center: ['60%', '50%'],
+                data: [
+                    {value:loser,},
+                    {value:100-loser,name:''}
+                ],
+                labelLine:{
+                    show:false
+                },
+                label:{
+                    show:true,
+                    position:'center',
+                    formatter:losername+':\n'+llabel,
+                    fontSize:15,
+                    backgroundColor:'rgba(255,255,255,0.5)'
+                },
+                emphasis:{
+                    
+                }
+            }
+        ]
+    };
+    return option;
+}
+
+function breakpointrcreate(statdata,id,winnername,losername){
+    for (var i=0;i<statdata.length;i++){
+        if(statdata[i].id==id){    
+            if(statdata[i].statslabel=='Break Points Converted')
+            {
+                winner=Number(statdata[i].wstat)
+                wlabel=statdata[i].winner
+                loser=Number(statdata[i].lstat)
+                llabel=statdata[i].loser
+            }
+            }
+        }
+    
+    option = {
+        title:{
+            text:'成功破发',
+            left:'auto'
+        },
+        series: [
+            {
+                name: 'winner',
+                type: 'pie',
+                radius: ['60%','80%'],
+                center: ['40%', '50%'],
+                data: [
+                    {value:winner},
+                    {value:100-winner}
+                ],
+                labelLine:{
+                    show:false
+                },
+                label:{
+                    show:true,
+                    position:'center',
+                    formatter:winnername+':\n'+wlabel,
+                    fontSize:15,
+                    backgroundColor:'rgba(255,255,255,0.5)'
+                },
+            },
+            {
+                name: 'loser',
+                type: 'pie',
+                radius: ['60%','80%'],
+                center: ['60%', '50%'],
+                data: [
+                    {value:loser,},
+                    {value:100-loser,name:''}
+                ],
+                labelLine:{
+                    show:false
+                },
+                label:{
+                    show:true,
+                    position:'center',
+                    formatter:losername+':\n'+llabel,
+                    fontSize:15,
+                    backgroundColor:'rgba(255,255,255,0.5)'
+                },
+                emphasis:{
+                    
+                }
+            }
+        ]
+    };
+    return option;
 }
